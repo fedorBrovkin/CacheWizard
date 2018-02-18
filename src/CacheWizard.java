f699d08cf3a8ebc5cafaccf1c1874a8833379e56
fedorBrovkin <brovkin_fedor@mail.ru>
2018-02-19 01:20:39 +0300

    emptyFrameCreated

--- src/CacheWizard.java
+++ src/CacheWizard.java
@@ -1,10 +1,22 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class CacheWizard extends JFrame{
-    private static final int DEFAULT_WIDTH=400;
+    private static final int DEFAULT_WIDTH=600;
     public static final int DEFAULT_HEIGHT=300;
-    public static void main(String[] args) {
+    private WizardFileManager wizard;
+    public CacheWizard(){
+        this.init();
+        wizard=new WizardFileManager();
+    }
+    public void init(){
+        this.setBounds(new Rectangle(DEFAULT_WIDTH,DEFAULT_HEIGHT));
+        this.setLocation(new Point(600,400));
 
+        this.setVisible(true);
+    }
+    public static void main(String[] args) {
+    CacheWizard wz=new CacheWizard();
 
     }
 
